582bd25f8ee501797e2889cec619a310bd8446e8
RoBeRt <robert@RoBeRttekiMacBook-Pro.local>
2013-01-16 17:26:45 +0800

    Initial Commit

---
+++ CoreGraphics/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreGraphics
+//
+//  Created by RoBeRt on 13-1-16.
+//  Copyright (c) 2013年 RoBeRt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreGraphics/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreGraphics
+//
+//  Created by RoBeRt on 13-1-16.
+//  Copyright (c) 2013年 RoBeRt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
